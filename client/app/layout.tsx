d9713db7e14016bc7789c5b58e44eaa729a448bd
TushitRoy <tushitroy2005@gmail.com>
2025-08-20 22:10:15 +0530

    Added Flashcards Page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@/app/globals.css";
+import "../app/globals.css";
 import { ThemeProvider } from "next-themes"
 
 const geistSans = Geist({
